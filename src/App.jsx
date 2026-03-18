bc348fbea1483b9b65dd44bab41ff66c34278a1d
Florian Gugele <florian.gugele@gmail.com>
2026-03-18 22:28:23 +0100

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='bg-gray-400 p-5'>
       <div className='bg-red-300'>Test</div>
+      <div className='text-3xl'>Bayern 3:0 Atalanta</div>
     </div>
   )
 }
