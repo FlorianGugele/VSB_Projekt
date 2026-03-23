d55a1998833c7c030257c091dc54b425e57f1d2c
Florian Gugele <florian.gugele@gmail.com>
2026-03-23 16:41:10 +0100

    header fertig ohne responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,16 @@
 import vsb from '../assets/vsb.jpg';
+import vsblogo from '../assets/vorarlberger-schützenbund.jpg';
 
 export default function Header() {
     return (
-        <div className="bg-white h-[7.5vh] items-center">
-            <div className='m-3'>
+        <div className="bg-white h-[23vh] flex flex-col justify-between">
+            <div className='m-5 ml-[4vw] mt-[3vh]'>
                 <a href="https://vlbg-sb.at" target="_blank" rel="noopener noreferrer">
-                    <img className="w-[150px]" src={vsb} alt="VSB Logo" />
+                    <img className="w-[175px]" src={vsblogo} alt="VSB Logo" />
                 </a>
             </div>
             <div>
-                <div className="bg-[#5f5f5f] h-[1vh]"></div>
+                <div className="bg-[#5f5f5f] h-[12vh]"></div>
             </div>
         </div>
     );
